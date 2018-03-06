820449bd1af94aa8f2880df67d6a93b63c3bb1c2
Shivam Mishra <scm.mymail@gmail.com>
2018-03-06 16:15:54 +0530

    Trying to make it a service 3

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ from faceswap import faceswap
 def my_webservice():
     name = request.form['employee_name']
     file = request.files['image']
-    # Uses faceswap
+    # Uses faceswap to change image
     name_of_output = faceswap(file, "head.jpg", name)
+
+    # Send this `url_for('static', filename=name_of_output)` in an HTTPS respose or any other form
     return url_for('static', filename=name_of_output)
+
 
 app.run(host="0.0.0.0", port=5000)
