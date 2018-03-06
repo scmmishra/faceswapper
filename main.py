a8c101dae6192e68ee5cba978d335c0bef216545
Shivam Mishra <scm.mymail@gmail.com>
2018-03-06 16:51:17 +0530

    Fixed Flask App

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
-import flask
+from flask import Flask
 from flask import send_file
 from faceswap import faceswap
 
 # faceswap(sys.argv[1], sys.argv[2])
+app = Flask(__name__)
 
 @app.route("/swap", methods=['POST'])
 def my_webservice():
@@ -15,5 +16,8 @@ def my_webservice():
     # Send this `url_for('static', filename=name_of_output)` in an HTTPS respose or any other form
     return url_for(filename=name_of_output)
 
+def main():
+    app.run(host="0.0.0.0", port=5000)
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    main()
